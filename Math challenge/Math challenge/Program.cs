d769cb6a79c571366981dfefce677ad368527150
CatherineMach <flowerstain@gmail.com>
2023-08-27 16:17:50 -0500

    assignments

---
+++ Math challenge/Math challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿int num1 = 5;
+int num2 = 7;
+int total = num1 + num2;
+Console.WriteLine(total);
+
+int num3 = 5;
+int num4 = 7;
+int difference = num3 - num4;
+Console.WriteLine(difference);
+
+int num5 = 5;
+int num6 = 7;
+int product = num5 * num6;
+Console.WriteLine(product);
+
+
+int num7 = 12;
+int num8 = 3;
+int quotient = num7 / num8;
+Console.WriteLine(quotient);
---
+++ anonymousIncomeComparison/anonymousIncomeComparison/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Anonymous Income Comparison Program");
+        Console.WriteLine("Person 1");
+        Console.WriteLine("Hourly Rate?");
+        Console.WriteLine("15");
+        Console.WriteLine("Hours worked per week?");
+        Console.WriteLine("40");
+        Console.WriteLine("Person 2");
+        Console.WriteLine("Hourly Rate?");
+        Console.WriteLine("20");
+        Console.WriteLine("Hours worked per week?");
+        Console.WriteLine("40");
+        Console.WriteLine("Annual salary of Person 1");
+        Console.WriteLine("31,200");
+        Console.WriteLine("Annual salary of Person 2");
+        Console.WriteLine("41,600");
+        Console.WriteLine("Does Person 1 make more money than Person 2?");
+        
+        int Person1 = 31200;
+        int Person2 = 41600;
+
+        bool moreMoney = Person1 > Person2;
+        Console.WriteLine(moreMoney);
+
+
+
+        Console.ReadLine();
+    }
+
+}
