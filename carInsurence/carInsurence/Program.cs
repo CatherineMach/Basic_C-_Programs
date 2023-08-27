a96f351d0602e78db2caed6f883e6d093b6f7cfe
CatherineMach <flowerstain@gmail.com>
2023-08-27 16:38:02 -0500

    insurence

---
+++ carInsurence/carInsurence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your age?");
+        Console.WriteLine("24");
+        Console.WriteLine("Have you ever had a DUI");
+        Console.WriteLine("No");
+        Console.WriteLine("How many speeding tickets do you have?");
+        Console.WriteLine("Zero");
+
+        int age = 24;
+        bool hadDUI = false;
+        bool tickets = false;
+
+        bool insurence = (age >= 15 && hadDUI && tickets);
+        Console.WriteLine(insurence);
+        Console.ReadLine();
+
+    }
+
+}
