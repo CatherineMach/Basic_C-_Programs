5d1f76205ec70645e100d8b17499052f83f28b41
CatherineMach <flowerstain@gmail.com>
2023-08-31 16:23:48 -0500

    edited car insurance

--- carInsurence/carInsurence/Program.cs
+++ carInsurence/carInsurence/Program.cs
@@ -5,17 +5,16 @@ class Program
     static void Main()
     {
         Console.WriteLine("What is your age?");
-        int age1 = Convert.ToInt32(Console.ReadLine());
+        int age = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Have you ever had a DUI");
         bool help = Convert.ToBoolean(Console.ReadLine());
         Console.WriteLine("How many speeding tickets do you have?");
         int ticket = Convert.ToInt32(Console.ReadLine());
 
-        int age = 24;
-        bool hadDUI = false;
-        bool tickets = false;
+        bool help = false;
+        int tickets = 3;
 
-        bool insurence = (age >= 15 && hadDUI && tickets);
+        bool insurence = (age >= 15 && help && tickets);
         Console.WriteLine(insurence);
         Console.ReadLine();
 
