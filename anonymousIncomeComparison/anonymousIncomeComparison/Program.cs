1fac82cd1e5cb65983e61b7f6811a5b265e7aa83
CatherineMach <flowerstain@gmail.com>
2023-08-31 12:02:05 -0500

    fixed the income comparison

--- anonymousIncomeComparison/anonymousIncomeComparison/Program.cs
+++ anonymousIncomeComparison/anonymousIncomeComparison/Program.cs
@@ -7,18 +7,18 @@ class Program
         Console.WriteLine("Anonymous Income Comparison Program");
         Console.WriteLine("Person 1");
         Console.WriteLine("Hourly Rate?");
-        Console.WriteLine("15");
+        int hourly = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Hours worked per week?");
-        Console.WriteLine("40");
+        int hours = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Person 2");
         Console.WriteLine("Hourly Rate?");
-        Console.WriteLine("20");
+        int hourly2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Hours worked per week?");
-        Console.WriteLine("40");
+        int hours2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Annual salary of Person 1");
-        Console.WriteLine("31,200");
+        int salary = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Annual salary of Person 2");
-        Console.WriteLine("41,600");
+        int salary2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Does Person 1 make more money than Person 2?");
         
         int Person1 = 31200;
