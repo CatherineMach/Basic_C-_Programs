e0591fa5afee43b441ea799934533642b08bdfc7
CatherineMach <flowerstain@gmail.com>
2023-08-31 16:09:40 -0500

    branching assignment

---
+++ ShippingPackage/ShippingPackage/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {   //title + weight of package
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+        Console.WriteLine("Please enter the package weight:");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+        }
+
+        //the package width
+        Console.WriteLine("Please enter the package width:");
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        if (width > 50) 
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+        }
+
+        //the package height
+       Console.WriteLine("Please enter the package height:");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        if (width > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+        }
+
+        //the package length
+        Console.WriteLine("Please enter the package length:");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        if (width > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+        }
+
+
+        Console.ReadLine();
+    }
+
+}
