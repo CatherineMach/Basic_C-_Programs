b43353b0ea87dae27512ac863235cf79d8b48d80
CatherineMach <flowerstain@gmail.com>
2023-09-04 14:29:30 -0500

    finished shipping package

--- ShippingPackage/ShippingPackage/Program.cs
+++ ShippingPackage/ShippingPackage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Schema;
 
 class Program
 {
@@ -12,36 +13,39 @@ class Program
         {
             Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
         }
-
-        //the package width
-        Console.WriteLine("Please enter the package width:");
-        int width = Convert.ToInt32(Console.ReadLine());
-
-        if (width > 50) 
+        else
         {
-            Console.WriteLine("Package too big to be shipped via Package Express.");
+
+            //the package width
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //the package height
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //the package length
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int totalDimensions = length + height + width;
+
+            if (totalDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+            int totalCost = totalDimensions * weight / 100;
+            }
+            Console.WriteLine("$" + totalCost);
+
+            Console.ReadLine();
         }
-
-        //the package height
-       Console.WriteLine("Please enter the package height:");
-        int height = Convert.ToInt32(Console.ReadLine());
-
-        if (width > 50)
-        {
-            Console.WriteLine("Package too big to be shipped via Package Express.");
-        }
-
-        //the package length
-        Console.WriteLine("Please enter the package length:");
-        int length = Convert.ToInt32(Console.ReadLine());
-
-        if (width > 50)
-        {
-            Console.WriteLine("Package too big to be shipped via Package Express.");
-        }
-
-
-        Console.ReadLine();
     }
 
 }
