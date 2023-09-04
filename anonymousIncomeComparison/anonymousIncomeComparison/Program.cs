49c137df5cd22720315f1214729b7835547eb99b
CatherineMach <flowerstain@gmail.com>
2023-09-04 14:41:29 -0500

    push

--- anonymousIncomeComparison/anonymousIncomeComparison/Program.cs
+++ anonymousIncomeComparison/anonymousIncomeComparison/Program.cs
@@ -5,23 +5,30 @@ class Program
     static void Main()
     {
         Console.WriteLine("Anonymous Income Comparison Program");
+
         Console.WriteLine("Person 1");
+
         Console.WriteLine("Hourly Rate?");
         int hourly = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Hours worked per week?");
         int hours = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Person 2");
+
         Console.WriteLine("Hourly Rate?");
         int hourly2 = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Hours worked per week?");
         int hours2 = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Annual salary of Person 1");
         int salary = Convert.ToInt32(Console.ReadLine());
 
         int salary1 = hourly * hours * 52;
         Console.WriteLine("Annual salary of Person 1: " + salary);
         Console.WriteLine("Annual salary of Person 2");
-        int salary2 = Convert.ToInt32(Console.ReadLine());
+        int salary11 = Convert.ToInt32(Console.ReadLine());
 
         int salary2 = hourly * hours * 52;
         Console.WriteLine("Annual salary of Person 2: " + salary);
@@ -30,7 +37,7 @@ class Program
 
 
 
-        bool moreMoney = salary > salary2;
+        bool moreMoney = salary11 > salary2;
         Console.WriteLine(moreMoney);
 
 
--- carInsurence/carInsurence/Program.cs
+++ carInsurence/carInsurence/Program.cs
@@ -6,15 +6,16 @@ class Program
     {
         Console.WriteLine("What is your age?");
         int age = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("Have you ever had a DUI");
-        bool help = Convert.ToBoolean(Console.ReadLine());
+        bool hadDUI = Convert.ToBoolean(Console.ReadLine());
+
         Console.WriteLine("How many speeding tickets do you have?");
-        int ticket = Convert.ToInt32(Console.ReadLine());
+        int tickets = Convert.ToInt32(Console.ReadLine());
 
-        bool help = false;
-        int tickets = 3;
 
-        bool insurence = (age >= 15 && help && tickets);
+
+        bool insurence = (age >= 15 && hadDUI && tickets);
         Console.WriteLine(insurence);
         Console.ReadLine();
 
