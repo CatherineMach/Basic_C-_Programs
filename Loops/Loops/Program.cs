7ea836ede9e029f7fc040a86809f5f1621fe3385
CatherineMach <flowerstain@gmail.com>
2023-09-05 16:12:49 -0500

    loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    //guessing a number
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isGuessed = number == 12;
+            do
+            {
+    //the switch that only breaks when you guess 12
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+    //defaults to saying "you are wrong" when guessing any other number
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+         }
+    }
+}
