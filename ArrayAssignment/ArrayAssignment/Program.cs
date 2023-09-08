575bb3011730d8de01428b7a98df1a0315c27b27
CatherineMach <flowerstain@gmail.com>
2023-09-08 13:26:28 -0500

    ArrayAssignment

---
+++ ArrayAssignment/ArrayAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+
+    static void Main()
+    {
+        string[] stringList = new string[] { "red", "blue", "green", "yellow" };
+        Console.WriteLine("Choose an index between 0 and 3");
+        int index1 = Convert.ToInt32(Console.ReadLine());
+
+        if (index1 < 0 || index1 >= stringList.Length)
+        {
+            Console.WriteLine("That index doesn't exist.");
+        }
+        else
+        {
+            Console.WriteLine(stringList[index1]);
+        }
+
+        List<string> stringList2 = new List<string>()
+        {
+            "square", "rectangle", "triangle", "circle"
+        };
+        Console.WriteLine("Choose an index between 0 and 3");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+        if (index2 < 0 || index2 >= stringList2.Count)
+        {
+            Console.WriteLine("That index doesn't exist.");
+        }
+        else
+        {
+            Console.WriteLine(stringList2[index2]);
+        }
+
+
+
+        Console.ReadLine();
+    }
+}
---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+        // Array Fundementals ^
+        // Lists v
+
+        List<int> intList = new List<int>();
+        intList.Add(4); //Or intList.Add("Hello");
+        intList.Add(10);
+        intList.Remove(10);
+
+        byte[] byteArray = new byte[5000];
+
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+    }
+}
