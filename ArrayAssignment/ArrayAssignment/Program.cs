655fb3801cec8f6f70afb863e6ef606f700c0019
CatherineMach <flowerstain@gmail.com>
2023-09-08 13:40:52 -0500

    array assignment

--- ArrayAssignment/ArrayAssignment/Program.cs
+++ ArrayAssignment/ArrayAssignment/Program.cs
@@ -20,6 +20,21 @@ class Program
             Console.WriteLine(stringList[index1]);
         }
 
+
+        int[] intList = new int[] { 1, 2, 3, 4, };
+        Console.WriteLine("Choose an index between 0 and 3");
+        int index3 = Convert.ToInt32(Console.ReadLine());
+
+        if (index3 < 0 || index3 >= intList.Length)
+        {
+            Console.WriteLine("That index doesn't exist.");
+        }
+        else
+        {
+            Console.WriteLine(intList[index3]);
+        }
+
+
         List<string> stringList2 = new List<string>()
         {
             "square", "rectangle", "triangle", "circle"
