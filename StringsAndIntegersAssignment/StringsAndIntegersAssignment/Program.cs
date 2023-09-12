15de5b0fa713e0553f83847264664ef3db94f446
CatherineMach <flowerstain@gmail.com>
2023-09-12 16:48:55 -0500

    Finished Assignment

--- StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -4,21 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] numList = { 32, 72, 98, 48, 50 };
-        Console.WriteLine("Write a number to divide some numbers with:)");
-        int userNum = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(numList[] + " divided by " + userNum + " equals... ");
+        List<int> numList = new List<int>{ 32, 72, 98, 48, 50 };
 
-        if (userNum < 0 || userNum >= numList.Length)
+        try
         {
-            Console.WriteLine();
+            Console.WriteLine("Write a number to divide some numbers with:)");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numList.Count; i++)
+            {
+                Console.WriteLine(numList[i] + " divided by " + userNum + " equals " + numList[i] / userNum);
+            }
+
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally 
+        { 
+            Console.WriteLine("Program has emerged from the try/catch block");
+            Console.ReadLine();
         }
 
 
 
-
-
-        Console.ReadLine();
     }
     
 }
