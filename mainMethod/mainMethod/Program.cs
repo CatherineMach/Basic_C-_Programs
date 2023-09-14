d6ec6e20676a856aa75b9b99f3835a59fa92ace8
CatherineMach <flowerstain@gmail.com>
2023-09-14 12:01:35 -0500

    mainMethod assignment

---
+++ mainMethod/mainMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+
+            Console.WriteLine("Enter a number to use for math equations");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number + " plus 5 is " + obj.Add(number));
+            Console.WriteLine(number + " subtract by 5 is " + obj.Subtract(number));
+            Console.WriteLine(number + " divided by 2 is " + obj.Divide(number));
+            Console.WriteLine(number + " multiplied by 2 is " + obj.Multiply(number));
+        }
+    }
+}
---
+++ mainMethod/mainMethod/myClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    public class myClass
+    {
+        public int Add(int num)
+        {
+            return num + 5;
+        }
+
+        public int Subtract(int num)
+        {
+            return num - 5;
+        }
+
+        public int Divide(int num)
+        {
+            return num / 2;
+        }
+
+        public int Multiply(int num)
+        {
+            return num * 2;
+        }
+    }
+}
