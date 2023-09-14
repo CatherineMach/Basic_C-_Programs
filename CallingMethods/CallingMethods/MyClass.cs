43c638b4420d9fc0de4ef02e93823f1788e741ff
CatherineMach <flowerstain@gmail.com>
2023-09-14 10:35:12 -0500

    calling methods assignment

---
+++ CallingMethods/CallingMethods/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MyClass
+    {
+        public int Addition(int num)
+        {
+            return num + 7;
+        }
+
+        public int Subtraction(int num)
+        {
+            return num - 7;
+        }
+
+        public int Multiply(int num)
+        {
+            return num * 7;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+
+            Console.WriteLine("Please enter a number to do some math on: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number + " plus 7 is " + obj.Addition(number));
+            Console.WriteLine(number + " subtracted by 7 is " + obj.Subtraction(number));
+            Console.WriteLine(number + " multiplied by 7 is " + obj.Multiply(number));
+
+            Console.ReadLine();
+        }
+    }
+}
