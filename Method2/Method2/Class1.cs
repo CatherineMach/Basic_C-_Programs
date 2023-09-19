c0af5cd81cd3a2303da3b31fd6ec04e2f590dddc
CatherineMach <flowerstain@gmail.com>
2023-09-19 15:09:30 -0500

    method assignment

---
+++ Method2/Method2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    public class Class1
+    {
+        public static void Sum(int firstNum, int secondNum, params object[] restOfNums)
+        {
+            int result = firstNum + secondNum;
+            if (secondNum != null)
+            {
+                foreach (int i in restOfNums)
+                   {
+                      result += i;
+                   }
+            }
+            
+
+            Console.WriteLine("Total is... " + result.ToString());
+        }
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ Method2/Method2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Methods
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("input a number");
+        int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("if you want input another number, if not press enter.");
+        int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+
+    }
+}
