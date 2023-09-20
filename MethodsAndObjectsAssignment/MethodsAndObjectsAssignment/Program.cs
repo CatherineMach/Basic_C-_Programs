500a17814931837bf25388903421babdb8a7e7b9
CatherineMach <flowerstain@gmail.com>
2023-09-19 20:24:23 -0500

    assignment

--- MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee employee = new Employee();
+            Person.SayName("Sample", "Student"); 
+            
+
+
+        }
+    }
+}
