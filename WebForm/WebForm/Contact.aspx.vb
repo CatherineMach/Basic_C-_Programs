a028917de291fad5a0f49450bb55d4133164b97f
CatherineMach <flowerstain@gmail.com>
2023-10-13 19:39:55 -0500

    web form assignment

---
+++ WebForm/WebForm/Contact.aspx.vb
@@ -0,0 +1,15 @@
+﻿Public Class Contact
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Protected Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
